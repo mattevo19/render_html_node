b70117c1e68824db6ced89d8e167de7002cb45d5
Matt Evans <mattevo10@gmail.com>
2022-05-19 14:45:27 +0200

    changed import to require

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import express from "express";
-const app = express();
-const path = require("path");
-const router = express.Router();
+const express = require("express")
+const app = express()
+const path = require("path")
+const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
-});
+  res.sendFile(path.join(__dirname + "/index.html"))
+})
 
 router.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname + "/about.html"));
-});
+  res.sendFile(path.join(__dirname + "/about.html"))
+})
 
-app.use("/", router);
+app.use("/", router)
 
 app.listen(3000, () => {
-  console.log("listening on 3000!!!");
-});
+  console.log("listening on 3000!!!")
+})
